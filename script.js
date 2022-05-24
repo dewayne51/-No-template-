15163955f56ee34d2e22e3459d87018a053d27b2
Dewayne Sewell <105006683+dewayne51@users.noreply.github.com>
2022-05-24 18:52:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,24 @@ test("There are at least 500 words on the page", () => {
 
 const console = document.getElementById("tests");
 prettify.toHTML(run(), console);
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('post').addEventListener('click', (event) => {
+  let element_unordered = document.getElementById('unordered');
+  let new_ul = document.createElement('ul');
+  new_ul.innerText = getNumberOrString(document.getElementById('comment-box').value);
+
+  element_unordered.appendChild(new_ul);
+
+});
